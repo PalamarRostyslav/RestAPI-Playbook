e3f89046941d355c7d7c38709d67a35078454bf6
Rostyslav Palamar <Ayncuin@gmail.com>
2025-06-02 23:58:53 +0300

    Initial commit

---
+++ RestApiCourse/Movies.Application/DI/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application.DI
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddSingleton<IMovieRepository,  MovieRepository>();
+
+            return services;
+        }
+    }
+}
